ada5782eb5678639de5693767bb6b949b5098ba1
Vigo Firmansyah Putra <61451159+vigouchiha@users.noreply.github.com>
2026-06-12 12:59:34 +0700

    lanjuttt

---
+++ _nuxt/w0SKPbUn.js
@@ -0,0 +1,1 @@
+import{v as s}from"./DqEZhwAy.js";const o=s("/borutotimeskip.jpg"),p=s("/youtube.png"),i=s("/tiktok.png"),r=s("/instagram.png"),_=s("/discord.png");export{o as _,p as a,i as b,r as c,_ as d};
